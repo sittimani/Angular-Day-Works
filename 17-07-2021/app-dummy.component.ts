48a4d6b9274de34c4c78fdc4faf88e6ab4756b5b
sittimani <67500879+sittimani@users.noreply.github.com>
2021-07-17 18:08:13 +0530

    17-07-2021 Work

---
+++ 17-07-2021/app-dummy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dummy',
+  template: '<div>Dummy Component</div>'
+})
+export class DummyComponent{
+    constructor(){
+
+    }
+}
